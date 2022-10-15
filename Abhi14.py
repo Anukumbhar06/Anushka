c380d6dafda906894ea12ae67d5250fe8436450b
Anukumbhar06 <115890627+Anukumbhar06@users.noreply.github.com>
2022-10-15 22:42:41 +0530

    Add files via upload

---
+++ Abhi14.py
@@ -0,0 +1,58 @@
+from turtle import*
+import turtle
+
+from zmq import HEARTBEAT_IVL
+colour("red")
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+
+
+
+
+output=❤(red HEART)
+
+
+
+imoort turtle()
+pen=turtel turtle()
+def curve()
+for i in range(200):
+    pen right(1)
+    pen.forward(1)
+    def heart():
+    pen.fillcolour('red')
+    pen beginfill()
+    pen left(140)
+    pen.forward(113)
+    curve()
+    pen.left(120)
+    curve()
+    pen.forward(112)
+    pen.endfill()
+    def txt():
+    pen.up()
+    pen.setpos(-68,95)
+    pen.down()
+    pen.color('light green') 
+    pen.write("isnt moon lovly abhimanyu infinity and foreve,")
+    font=("verdan",15,"bold") 
+    heart()
+    text()
+    pen.ht()
+    turtle
+    turtle.exiton
+    click()  
+
+
+
+
+    output=(U+2764)💕💋
+
+
